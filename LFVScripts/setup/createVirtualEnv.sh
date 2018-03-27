2b1713ede878d5def75d7e86a11835e7314f6997
Brian Dorney <brian.l.dorney@cern.ch>
2018-03-27 17:23:08 +0200

    fixed a bug in the venv creation script that tried to install packages to the system instead of the venv

--- LFVScripts/setup/createVirtualEnv.sh
+++ LFVScripts/setup/createVirtualEnv.sh
@@ -12,12 +12,15 @@ then
     return
 fi
 
+#pip install --user virtualenv
+
 # Check if default virtualenv exists
 VENV_BASE=$CMSSW_BASE/src/LFVAnalysis
 if [ ! -d $VENV_BASE/default ]
 then
     echo "Making default virtualenv, this may take some time"
     virtualenv $VENV_BASE/default -p python --system-site-packages
+    source $VENV_BASE/default/bin/activate
     pip install --upgrade pip
     pip install -U numpy root_numpy ipython
     #deactivate
